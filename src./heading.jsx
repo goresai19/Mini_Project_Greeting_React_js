6abf8f223afa8063443e559186a93376c3ba742f
goresai19 <100403114+goresai19@users.noreply.github.com>
2022-12-25 13:34:11 +0530

    Create heading.jsx

---
+++ src./heading.jsx
@@ -0,0 +1,14 @@
+// importing the react js from the react
+import React from "react";
+
+//Creating a function heading which we have to return the heading message.
+function Heading()
+{   
+    return(
+    <h1>Hi, I am Sainath Gore</h1>
+    );
+}
+// Exporting the function heading 
+export default Heading;
+
+
