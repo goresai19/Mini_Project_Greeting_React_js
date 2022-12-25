7212321b766663fd4cca83b3c74118d5481122ae
goresai19 <100403114+goresai19@users.noreply.github.com>
2022-12-25 13:35:14 +0530

    Create para.jsx

---
+++ src./para.jsx
@@ -0,0 +1,10 @@
+//imorting the react fromt the react.
+import React from "react";
+
+//Creating the function subheading to display the subheading.
+function SubHeading()
+{   
+    return(<h2>Mini-project showing the time depend upon the time of the system.</h2>);
+}
+// Exporing the function subheading.
+export default SubHeading;
